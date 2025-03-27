af6162d1bd83767135f9e455d606720ccdf4ee41
Rossskkki <ros707821@163.com>
2025-03-27 19:23:12 +0800

    feat: 添加附近公交车站显示及详细信息功能
    在index.html中添加了信息展示容器，并在style.css中定义了相关样式。在main.js中实现了获取附近公交车站的功能，并根据距离排序显示。同时，添加了点击车站名称获取详细信息的交互功能，通过调用API获取公交车站的实时信息并展示。

--- main.js
+++ main.js
@@ -30,11 +30,62 @@ getloc().then(location => {
     current_lat = current_location[0];
     current_lon = current_location[1];
 
-    current_lat = 22.28314
-    current_lon = 114.13716
-    get_nearby_bus_stops(current_lat, current_lon).then(nearby_bus_stops => {
-        console.log("Nearby Bus Stops:", nearby_bus_stops); 
-    })
+    current_lat = 22.28399
+    current_lon = 114.13333
+    let selection = document.getElementById("distance");
+
+    async function updateBusStops() {
+        try {
+            const nearby_bus_stops = await get_nearby_bus_stops(current_lat, current_lon, selection.value);
+            console.log("Nearby Bus Stops:", nearby_bus_stops);
+            // Update UI
+            if (nearby_bus_stops.length === 0) {
+               let output = "<p>Cannot locate nearby bus stops</p>";
+               document.getElementById("info-container").innerHTML = output;
+               return;
+            }
+            let output = '<div class="bus-stops-container">';
+            for (let i = 0; i < nearby_bus_stops.length; i++) {
+                let bus_stop = nearby_bus_stops[i];
+                output += `
+                    <div class="bus-stop-item">
+                        <div class="distance-info">Distance: ${bus_stop.distance}m</div>
+                        <div class="stop-info">Stop: <span id="${bus_stop.stop}" class="stop-name">${bus_stop.name_en}</span></div>
+                    </div>
+                    <div id="stop_${bus_stop.stop}" class="stop-detail"></div>
+                `;
+            }
+            output += '</div>';
+            document.getElementById("info-container").innerHTML = output;
+            
+            // add event listener to bus stops
+            let bus_stop_items = document.getElementsByClassName("stop-name");
+            for (let i = 0; i < bus_stop_items.length; i++) {
+                let bus_stop_item = bus_stop_items[i];
+                bus_stop_item.addEventListener("click", display_bus_stop_info); 
+            }
+        } catch (error) {
+            console.error("Error updating bus stops:", error);
+        }
+    }
+
+    async function display_bus_stop_info(event) {
+        try {
+            const stop_id = event.target.id;
+            const bus_stop_info = await get_bus_stop_info(stop_id);
+            console.log("Bus Stop Info:", bus_stop_info); 
+        } 
+        catch (error) {
+            console.error("Error displaying bus stop info:", error); 
+        }
+    }
+
+    // initial update
+    updateBusStops();
+
+    // add event listener to selection
+    selection.addEventListener("change", updateBusStops);
+
 
 }).catch(err => {
     console.error(err);
@@ -56,8 +107,7 @@ function get_distance_diff(lat1, lon1, lat2, lon2){
     return d;
 }
 
-async function get_nearby_bus_stops(current_lat, current_lon){
-    let limit_distance = document.getElementById("distance").value;
+async function get_nearby_bus_stops(current_lat, current_lon,limit_distance){
     let nearby_bus_stops = [];
     
     // get all bus stops from session storage
@@ -89,9 +139,11 @@ async function get_nearby_bus_stops(current_lat, current_lon){
         // console.log("Distance:", distance);
         // console.log("Limit Distance:", limit_distance);
         if(distance <= limit_distance){
+            bus_stop.distance = distance.toFixed(0);
             nearby_bus_stops.push(bus_stop);
         }
     }
+    nearby_bus_stops.sort((a, b) => a.distance - b.distance);
     return nearby_bus_stops;
 }
 
@@ -101,4 +153,37 @@ function create_session_storage(key,data){
 
 function get_session_storage(key){
     return sessionStorage.getItem(key);
+}
+
+async function get_bus_stop_info(stop_id){
+    try {
+        const response = await fetch(`https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/${stop_id}`);
+        const JSONData = await response.json();
+        const data = JSONData.data;
+        console.log("Bus Stop Row Info:", data);
+        const resData = {};
+        
+        for (let i = 0; i < data.length; i++) {
+            const { route, service_type, dir, dest_en, eta } = data[i];
+            // 使用路线号码和方向创建唯一标识
+            const routeKey = `${route}_${dir}`;
+            
+            if (eta === null) continue;
+            
+            if (resData[routeKey]) {
+                if (service_type === resData[routeKey][2]) {
+                    resData[routeKey][0].push(eta);
+                } else {
+                    console.log("Different Service Type");
+                }
+                continue;
+            }
+            
+            resData[routeKey] = [[eta], dir, service_type, dest_en];
+        }
+        return resData;
+    } catch (error) {
+        console.error("Error fetching bus stop info:", error);
+        return;
+    }
 }
